a81c656be0480a160d9e2e8ff169f87d78f83576
Jeremy <jeremy.rodrguez@yahoo.com>
2020-11-18 21:32:22 -0800

    commit tree 1a7b1f0dc885f4a9c94fdb1e686a1549221bca7e

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+class CashRegister
+    attr_accessor :total, :discount
 
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = 0
+    end
+
+    def add_item(title, price, quantity = nil)
+    end
+
+    def apply_discount
+    end
+
+    def items
+    end
+
+    def void_last_transaction
+    end
+end
